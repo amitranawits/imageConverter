0d5719ef240c9a6ede4d0bf3ea3cf855b779c8d4
Amit Rana <amitrana@WIL-Admins-MacBook-Air.local>
2021-02-26 17:26:19 +0530

    adding test cases

--- utils/__test__/server.js
+++ utils/__test__/server.js
@@ -2,7 +2,7 @@ process.env.NODE_ENV = 'test';
 
 let chai = require('chai');
 let chaiHttp = require('chai-http');
-let server = require('../index');
+let server = require('../../index');
 var should = require('chai').should();
 const path = require("path");
 
